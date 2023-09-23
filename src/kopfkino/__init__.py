c8d0c95ec8e92f276bf49ea33e4ec13a2843c2c3
jhermann <jh@web.de>
2023-09-23 18:41:23 +0200

    add copier-pdm project files

---
+++ src/kopfkino/__init__.py
@@ -0,0 +1,8 @@
+"""kopfkino package.
+
+Syntactic sugar sprinkled on top of MoviePy and AI components to allow authoring simple human-friendly video scripts.
+"""
+
+from __future__ import annotations
+
+__all__: list[str] = []
---
+++ src/kopfkino/cli.py
@@ -0,0 +1,42 @@
+"""Module that contains the command line application."""
+
+# Why does this file exist, and why not put this in `__main__`?
+#
+# You might be tempted to import things from `__main__` later,
+# but that will cause problems: the code will get executed twice:
+#
+# - When you run `python -m kopfkino` python will execute
+#   `__main__.py` as a script. That means there won't be any
+#   `kopfkino.__main__` in `sys.modules`.
+# - When you import `__main__` it will get executed again (as a module) because
+#   there's no `kopfkino.__main__` in `sys.modules`.
+
+from __future__ import annotations
+
+import argparse
+
+
+def get_parser() -> argparse.ArgumentParser:
+    """Return the CLI argument parser.
+
+    Returns:
+        An argparse parser.
+    """
+    return argparse.ArgumentParser(prog="kopfkino")
+
+
+def main(args: list[str] | None = None) -> int:
+    """Run the main program.
+
+    This function is executed when you type `kopfkino` or `python -m kopfkino`.
+
+    Parameters:
+        args: Arguments passed from the command line.
+
+    Returns:
+        An exit code.
+    """
+    parser = get_parser()
+    opts = parser.parse_args(args=args)
+    print(opts)
+    return 0
